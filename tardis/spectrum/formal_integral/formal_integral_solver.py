2f6b521a5dcc7977e3e37eb22f850c4e70eed0f0
Haille <hmlperkins@hotmail.com>
2025-07-03 21:29:26 -0500

    import sourcefunctionsolver

--- tardis/spectrum/formal_integral/formal_integral_solver.py
+++ tardis/spectrum/formal_integral/formal_integral_solver.py
@@ -1,14 +1,49 @@
 
 from tardis.spectrum.base import TARDISSpectrum
+from tardis.spectrum.formal_integral.source_function_solver import SourceFunctionSolver
 
 
 def FormalIntegralSolver():
 
 
-    def __init__(self, configuration):
+    def __init__(self, formal_integral_configuration, spectrum_configuration, integrator_configuration):
 
-        self.configuration = configuration
+        # will need configurations for
+            # which method to use (numba, cuda, etc)
+            # number of shells and points
+            # spectrum configuration
+        self.formal_integral_configuration = formal_integral_configuration
+        self.spectrum_configuration = spectrum_configuration
+        self.integrator_configuration = integrator_configuration
 
     def solve(self):
 
+        points = self.integrator_configuration.points
+        interpolate_shells = self.integrator_configuration.interpolate_shells
+
+        att_S_ul, Jredlu, Jbluelu, e_dot_u = SourceFunctionSolver().solve()
+
         return TARDISSpectrum()
+    
+    def setup_optional(method):
+
+        """
+        Set up the integrator depending on the method specified in the configuration.
+
+        Parameters
+        ----------
+        method : str
+            The method to use for the formal integral solver, e.g., 'numba', 'cuda'.
+
+        Returns
+        -------
+        integrator : FormalIntegrator
+            An instance of the appropriate formal integrator.
+        """
+
+
+
+        pass # TODO: Implement method to set up the integrator based on the configuration.
+
+    
+
