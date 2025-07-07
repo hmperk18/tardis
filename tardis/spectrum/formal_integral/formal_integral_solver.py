dc9b9c8951e70a1ed8d269d8403108cd4d445b0d
Haille <hmlperkins@hotmail.com>
2025-07-07 10:41:12 -0500

    placeholder logic for use in the formal_integral

--- tardis/spectrum/formal_integral/formal_integral_solver.py
+++ tardis/spectrum/formal_integral/formal_integral_solver.py
@@ -21,9 +21,25 @@ def FormalIntegralSolver():
         points = self.integrator_configuration.points
         interpolate_shells = self.integrator_configuration.interpolate_shells
 
-        att_S_ul, Jredlu, Jbluelu, e_dot_u = SourceFunctionSolver().solve()
 
-        return TARDISSpectrum()
+        # TODO: determine configs for SourceFunctionSolver
+        sourceFunction = SourceFunctionSolver(solver_configs)
+        att_S_ul, Jredlu, Jbluelu, e_dot_u = sourceFunction.solve()
+
+        # interpolate if necessary
+        # TODO: generalize the interpolation function
+        # e.g. 
+            # r_inner_i = interpolate(...)
+
+        # TODO: compute the formal integral
+        # 
+        # L, I_nu_p = self.integrator.formal_integral()
+        
+        # TODO: make a TARDISSpectrum object from the results    
+        frequency = ...  # Extract frequency from the results
+        luminosity = ...  # Extract luminosity from the results
+        spec = TARDISSpectrum(frequency, luminosity)
+        return spec
     
     def setup_optional(method):
 
